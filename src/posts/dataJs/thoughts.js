72894fc31b2e69323a94ddc609d07f1c6cd3f4c2
HYTHT-max <you@example.com>
2026-02-11 18:57:10 +0800

    first commit

--- src/posts/dataJs/thoughts.js
+++ src/posts/dataJs/thoughts.js
@@ -4,6 +4,15 @@ export const thoughts = [
         "likes": 0,
         "comments": 0,
         "content": "测试",
+        "id": 1770807422863,
+        "week": "星期三",
+        "_cms_id": "1770807422863-khc3r0iyv"
+    },
+    {
+        "date": "2026-02-11",
+        "likes": 0,
+        "comments": 0,
+        "content": "测试",
         "id": 1770807075001,
         "week": "星期三",
         "_cms_id": "1770807075001-yl1d70mm5"
